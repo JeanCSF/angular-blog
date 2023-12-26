2b4e5056754b79dcdec861ac131f4a862e65f2f1
JeanCSF <jean.csf.17@gmail.com>
2023-12-26 20:33:39 -0300

    projeto base criado

---
+++ src/app/data/dataFake.ts
@@ -0,0 +1,23 @@
+export const dataFake = [
+  {
+    id: "1",
+    photo: 'https://picsum.photos/450',
+    title: 'Lorem Ipsum',
+    description:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa, non.',
+  },
+  {
+    id: "2",
+    photo: 'https://picsum.photos/451',
+    title: 'Lorem Ipsum',
+    description:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa, non.',
+  },
+  {
+    id: "3",
+    photo: 'https://picsum.photos/452',
+    title: 'Lorem Ipsum',
+    description:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa, non.',
+  }
+];
